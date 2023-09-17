bd937beb1202c51544bf326320eabf5406240348
Ching Cheng Kang <pycck@hotmail.com>
2023-09-17 20:13:59 +0800

    improve factory

--- database/factories/QuoteFactory.php
+++ database/factories/QuoteFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Author;
 use App\Models\Quote;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 
@@ -10,12 +12,12 @@ class QuoteFactory extends Factory
 {
     protected $model = Quote::class;
 
-    public function definition()
+    public function definition(): array
     {
         return [
-            'content' => $this->faker->word(),
-            'author_id' => $this->faker->randomNumber(),
-            'user_id' => $this->faker->word(),
+            'content' => $this->faker->realText(),
+            'author_id' => Author::factory(),
+            'user_id' => User::factory(),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
