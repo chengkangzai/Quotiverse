741def3c7237cbd5320d0ae4729a3972741f19b0
Ching Cheng Kang <pycck@hotmail.com>
2023-09-17 20:15:09 +0800

    add relationship for model

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Quote extends Model
@@ -16,4 +17,14 @@ class Quote extends Model
         'author_id',
         'user_id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
