f67e4dfa751b53a16627d9bf2466362a3a3377e4
chengkangzai <pycck@hotmail.com>
2025-05-25 22:02:53 +0800

    Configure TextColumn placeholder in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        TextColumn::configureUsing(function (TextColumn $column) {
+            $column->placeholder('N/A');
+        });
     }
 }
